75304c9ff35e5719d98c7901c033cfd4d9f700b3
Rattan Singh <rattansingh03@outlook.com>
2026-04-06 12:30:34 -0400

    added functions in GroupOfCards class

--- src/ca/sheridancollege/project/GroupOfCards.java
+++ src/ca/sheridancollege/project/GroupOfCards.java
@@ -17,20 +17,14 @@ import java.util.Collections;
  * @author Prem Parashar
  */
 public class GroupOfCards {
-
-    //The group of cards, stored in an ArrayList
     private ArrayList<Card> cards;
-    private int size;//the size of the grouping
+    private int size;
 
     public GroupOfCards(int size) {
         this.size = size;
+        this.cards = new ArrayList<>();
     }
 
-    /**
-     * A method that will get the group of cards as an ArrayList
-     *
-     * @return the group of cards.
-     */
     public ArrayList<Card> getCards() {
         return cards;
     }
@@ -39,18 +33,11 @@ public class GroupOfCards {
         Collections.shuffle(cards);
     }
 
-    /**
-     * @return the size of the group of cards
-     */
     public int getSize() {
-        return size;
+        return cards.size();
     }
 
-    /**
-     * @param size the max size for the group of cards
-     */
     public void setSize(int size) {
         this.size = size;
     }
-
-}//end class
+}
